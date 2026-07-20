a39b86f3e60f39f364610c5bef99bcfb62d54f1a
hansrajathole <hansrajathole1@gmail.com>
2026-07-20 15:44:25 +0530

    database uri update

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 
 const connect = async()=>{
-    await mongoose.connect("mongodb://localhost:27017/socket")
+    await mongoose.connect("mongodb+srv://kumarachintya700_db_user:RgUEJLMbN4NPJmSV@cluster0.g4fah6q.mongodb.net/socket")
      .then(()=>{
         console.log("database connected..");
         
